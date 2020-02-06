eab80cdaa95637d158e31e245af5de07c97a1c3f
Dalyer <dseiler@sfu.ca>
2020-02-05 20:40:59 -0800

    added fab to WorkoutCreator

--- app/src/main/java/com/example/david/amelioration/WorkoutCreatorActivity.java
+++ app/src/main/java/com/example/david/amelioration/WorkoutCreatorActivity.java
@@ -1,9 +1,12 @@
 package com.example.david.amelioration;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.LinkedList;
 
@@ -18,7 +21,25 @@ public class WorkoutCreatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_creator);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int exerciseListSize = mExerciseList.size();
+                // add an item to this list
+                Exercise x = new Exercise("test trash");
+                mExerciseList.addLast(x);
+                mRecyclerView.getAdapter().notifyItemInserted(exerciseListSize-1);
+                mRecyclerView.smoothScrollToPosition(exerciseListSize-1);
+            }
+            });
+
         // Put dummy data into exercise list
+        // TODO ultimately this won't be a thing everything will be added from the button
         for (int i = 0; i < 20; i++) {
             String tmp = "exercise" + i;
             Exercise x = new Exercise(tmp);
