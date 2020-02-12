2f753e19599c7f211b93c8cd528d82ecace4dfff
Dalyer <dseiler@sfu.ca>
2020-02-11 19:08:05 -0800

    added schedule Repository

---
+++ app/src/main/java/com/example/david/amelioration/ScheduleRepository.java
@@ -0,0 +1,42 @@
+package com.example.david.amelioration;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class ScheduleRepository {
+    // Member variables
+    private ScheduleDao mScheduleDao;
+    private LiveData<List<Schedule>> mAllSchedules;
+
+    public ScheduleRepository(Application application) {
+        ScheduleRoomDatabase db = ScheduleRoomDatabase.getDatabase(application);
+        mScheduleDao = db.scheduleDao();
+        mAllSchedules = mScheduleDao.getAllSchedules();
+    }
+
+    LiveData<List<Schedule>> getAllSchedules() {
+        return mAllSchedules;
+    }
+
+    public void insert(Schedule schedule) {
+        new insertAsyncTask(mScheduleDao).execute(schedule);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Schedule, Void, Void> {
+
+        private ScheduleDao mAsyncTaskDao;
+
+        insertAsyncTask(ScheduleDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Schedule... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
