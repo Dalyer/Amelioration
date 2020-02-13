3d47a37e82377ea1fb1549a243575ea995c8ac3f
Dalyer <dseiler@sfu.ca>
2020-02-12 21:06:44 -0800

    added clear database button and alert to settings

--- app/src/main/java/com/example/david/amelioration/SettingsActivity.java
+++ app/src/main/java/com/example/david/amelioration/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.david.amelioration;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v7.app.AlertDialog;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,4 +13,29 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    public void clearDatabase(View view) {
+        AlertDialog.Builder clearDatabaseAlert = new AlertDialog.Builder(SettingsActivity.this);
+
+        clearDatabaseAlert.setTitle("Warning");
+        clearDatabaseAlert.setMessage("Are you sure you want to clear the database?");
+
+        // dialog buttons
+        clearDatabaseAlert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // clear the database
+            }
+        });
+        clearDatabaseAlert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        });
+        // Create and show alert
+        clearDatabaseAlert.show();
+
+    }
 }
+
