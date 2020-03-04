0e50d6a51c4e42509511e5a68d89b3d6335532a4
Dalyer <dseiler@sfu.ca>
2020-03-04 13:01:29 -0800

    added one view model to rule them all

---
+++ app/src/main/java/com/example/david/amelioration/WorkoutCreatorViewModel.java
@@ -0,0 +1,51 @@
+package com.example.david.amelioration;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+public class WorkoutCreatorViewModel extends AndroidViewModel {
+    private ScheduleRepository mRepo;
+    // Schedule
+    private LiveData<List<Schedule>> mAllSchedules;
+    // Day
+    private LiveData<List<Day>> mAllMatchingDays;
+    // Exercise
+    private LiveData<List<Exercise>> mAllMatchingExercises;
+
+
+    public WorkoutCreatorViewModel(@NonNull Application application, int scheduleId, int dayId, int exerciseId) {
+        super(application);
+        mRepo = new ScheduleRepository(application, scheduleId, dayId, exerciseId);
+        mAllSchedules = mRepo.getAllSchedules();
+        mAllMatchingDays = mRepo.getmAllMatchingDays();
+        mAllMatchingExercises = mRepo.getmAllMatchingExercises();
+    }
+
+    LiveData<List<Schedule>> getAllSchedules () {
+        return mAllSchedules;
+    }
+
+    LiveData<List<Day>> getmAllMatchingDays() {
+        return mAllMatchingDays;
+    }
+
+    LiveData<List<Exercise>> getmAllMatchingExercises() {
+        return mAllMatchingExercises;
+    }
+
+    public void insert(Schedule schedule) {
+        mRepo.insert(schedule);
+    }
+
+    public void insert(Day day) {
+        mRepo.insert(day);
+    }
+
+    public void insert(Exercise exercise) {
+        mRepo.insert(exercise);
+    }
+}
